1d47f730658a7d63c5bd4fcf30483319bfdc8875
Tutanabg <66039997+Tutanabg@users.noreply.github.com>
2020-12-11 15:55:17 -0500

    Update GoogleJavaScript.js

--- GoogleJavaScript.js
+++ GoogleJavaScript.js
@@ -2,7 +2,8 @@ class App extends React.Component{
 	render(){
 	  return(
 		<div>
-		 <h1>This Website is designed to create simple Google Search websites.</h1>
+                 <h1>Welcome!</h1>
+		 <h2>This Website is designed to create simple Google Search websites.</h2>
 		</div>
                 );
                 }
